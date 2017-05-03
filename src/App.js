0ba9bd61399ff10e696b484deda1e55a731b5b9c
felixerdy <felix.erdmann@uni-muenster.de>
2017-05-03 22:54:13 +0200

    update contact field

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import CircularImage from './Components/CircularImage.js'
 import ListDetails from './Components/ListDetails.js'
 import LifestylePhotoList from './Components/LifestylePhotoList.js'
-
+var Columns = require('react-columns');
+import Detail from './Components/Detail.js'
 
 import './style/App.css';
 
@@ -16,18 +17,27 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <CircularImage path='./Images/profile.jpg' />
+          <CircularImage path='./Images/profile.jpg'/>
           <h1>&lt; Hey, I'm Felix &gt;</h1>
         </div>
         <div className="about-me">
           <h1>About me</h1>
           <hr width="100px;"></hr>
           <p>
-            Hey, I am Felix and I am living in <a href="https://www.google.de/maps/place/Münster">Münster, Germany 🚲🌧️</a>. I am studying Geoinformatics 🌍💻 at the <a href="https://www.uni-muenster.de/Geoinformatics/en/">Institute for Geoinformatics</a>. Besides that, I am working in the <a href="https://sensebox.de/en/">senseBox</a> team, where I work on a new website, on Arduino projects or do workshops with pupils. I like to work on frontend projects but I am also interested in backend technologies.
+            Hey, I am Felix and I am living in
+            <a href="https://www.google.de/maps/place/Münster">Münster, Germany 🚲🌧️</a>. I am studying Geoinformatics 🌍💻 at the <a href="https://www.uni-muenster.de/Geoinformatics/en/">Institute for Geoinformatics</a>. Besides that, I am working in the <a href="https://sensebox.de/en/">senseBox</a> team, where I work on a new website, on Arduino projects or do workshops with pupils. I like to work on frontend projects but I am also interested in backend technologies.
           </p>
           <p>
             In my freetime I enjoy <a href="https://skylines.aero/users/999">gliding ✈️</a>, riding my bicycle 🚲 or exploring new things.
           </p>
+          <Columns columns={2}>
+            <a href="https://github.com/felixerdy" className="contact-icon">
+              <div className="devicons devicons-github_badge"></div>
+            </a>
+            <a href="mailto:felixaetem@googlemail.com" className="contact-icon">
+              <img src="./Images/mail.svg"></img>
+            </a>
+          </Columns>
         </div>
         <div className="tech-details">
           <h1>My interests</h1>
